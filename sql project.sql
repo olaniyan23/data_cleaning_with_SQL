5d1d230d4c4f7553528c3361d32f94a1284cddb8
Olaniyan Blessing <olaniyanblessing16@gmail.com>
2023-03-28 14:40:57 +0100

    modified the sql file

--- sql project.sql
+++ sql project.sql
@@ -1,95 +1,96 @@
-select*
-from dbo.NashvilleHousing;
+select *
+from portfolio.dbo.Nashville_Housing;
 
 -- converting the saledate format
-alter table dbo.NashvilleHousing
+alter table portfolio.dbo.Nashville_Housing
 add SaledateConverted date;
 
-update dbo.NashvilleHousing
-set SaledateConverted= convert(date, SaleDate);
+update portfolio.dbo.Nashville_Housing
+set SaledateConverted= convert(date, Nashville_Housing.SaleDate);
 
-select SaleDate, SaledateConverted
-from dbo.NashvilleHousing;
+select Nashville_Housing.SaleDate, SaledateConverted
+from portfolio.dbo.Nashville_Housing;
+
 
 -- cleaning property address
 select *
-from dbo.NashvilleHousing
+from portfolio.dbo.NashvilleHousing
 order by ParcelID
 
 select a.ParcelID, a.propertyAddress, b.ParcelID, b.PropertyAddress, 
 ISNULL(a.PropertyAddress,b.PropertyAddress)as new_propertyAddress
-from dbo.NashvilleHousing a
-join dbo.NashvilleHousing b
+from portfolio.dbo.Nashville_Housing as a
+join portfolio.dbo.Nashville_Housing as b
 	on a.ParcelID=b.ParcelID
 	and a.[UniqueID ]<> b.[UniqueID ]
-where a.PropertyAddress is null
+where a.PropertyAddress is null;
 
 update a
 set propertyAddress = ISNULL(a.PropertyAddress,b.PropertyAddress)
-from dbo.NashvilleHousing a
-join dbo.NashvilleHousing b
+from portfolio.dbo.Nashville_Housing a
+join portfolio.dbo.Nashville_Housing b
 	on a.ParcelID=b.ParcelID
 	and a.[UniqueID ]<> b.[UniqueID ]
 where a.PropertyAddress is null;
 
 select PropertyAddress
-from dbo.NashvilleHousing
+from portfolio.dbo.Nashville_Housing
 
 select 
 SUBSTRING(PropertyAddress,1, charindex(',', PropertyAddress)-1) as address,
 SUBSTRING(PropertyAddress, charindex(',', PropertyAddress)+1, len(propertyAddress)) as address
 
-from dbo.NashvilleHousing;
+from portfolio.dbo.Nashville_Housing;
 
-Alter table dbo.NashvilleHousing
+Alter table portfolio.dbo.Nashville_Housing
 add propertysplitAddress nvarchar(255)
 
-update dbo.NashvilleHousing
+update portfolio.dbo.Nashville_Housing
 set propertysplitAddress = SUBSTRING(PropertyAddress,1, charindex(',', PropertyAddress)-1)
 
-Alter table dbo.NashvilleHousing
+Alter table portfolio.dbo.Nashville_Housing
 add propertysplitcity nvarchar(255)
 
-update dbo.NashvilleHousing
+update portfolio.dbo.Nashville_Housing
 set propertysplitcity = SUBSTRING(PropertyAddress, charindex(',', PropertyAddress)+1, len(propertyAddress));
 
 select propertysplitAddress, propertysplitcity
-from dbo.NashvilleHousing
+from portfolio.dbo.Nashville_Housing
 
 -- cleaning owner address
 select OwnerAddress
-from dbo.NashvilleHousing
+from portfolio.dbo.Nashville_Housing
 
 select 
 PARSENAME(replace(OwnerAddress,',','.'),3),
 PARSENAME(replace(OwnerAddress,',','.'),2),
 PARSENAME(replace(OwnerAddress,',','.'),1)
-from dbo.NashvilleHousing
+from portfolio.dbo.Nashville_Housing
 
-Alter table dbo.NashvilleHousing
+Alter table portfolio.dbo.Nashville_Housing
 add OwnersplitAddress nvarchar(255);
 
-update dbo.NashvilleHousing
+update portfolio.dbo.Nashville_Housing
 set OwnersplitAddress =PARSENAME(replace(OwnerAddress,',','.'),3);
 
-Alter table dbo.NashvilleHousing
+Alter table portfolio.dbo.Nashville_Housing
 add Ownersplitcity nvarchar(255)
 
-update dbo.NashvilleHousing
+update portfolio.dbo.Nashville_Housing
 set Ownersplitcity = PARSENAME(replace(OwnerAddress,',','.'),2)
 
-Alter table dbo.NashvilleHousing
+Alter table portfolio.dbo.Nashville_Housing
 add Ownersplitstate nvarchar(255)
 
-update dbo.NashvilleHousing
+update portfolio.dbo.Nashville_Housing
 set Ownersplitstate =PARSENAME(replace(OwnerAddress,',','.'),1);
 
 select ownerAddress, ownersplitAddress,Ownersplitcity,ownersplitstate
-from dbo.NashvilleHousing;
+from portfolio.dbo.Nashville_Housing;
 
 -- cleaning sold as vacant column
 select distinct(soldasvacant), count(soldasvacant)
-from dbo.NashvilleHousing
+from portfolio.dbo.Nashville_Housing
 group by SoldAsVacant order by 2 desc;
 
 -- using case statement
@@ -99,18 +100,18 @@ when soldasvacant = 'Y' then 'Yes'
 when soldasvacant = 'N' then 'No'
 else soldasvacant
 end as soldasvacant
-from dbo.NashvilleHousing
+from portfolio.dbo.Nashville_Housing
 
-update dbo.NashvilleHousing
+update portfolio.dbo.Nashville_Housing
 set SoldAsVacant= case
 when soldasvacant = 'Y' then 'Yes'
 when soldasvacant = 'N' then 'No'
 else soldasvacant
 end 
-from dbo.NashvilleHousing;
+from portfolio.dbo.Nashville_Housing;
 
 select distinct(SoldAsVacant)
-from dbo.NashvilleHousing;
+from portfolio.dbo.Nashville_Housing;
 
 -- remove duplicate
 with row_num as (
@@ -118,17 +119,11 @@ select*,
 ROW_NUMBER() over(
 partition by parcelID,propertyAddress, Saleprice, SaledateConverted,legalreference 
 order by uniqueID) as RowNum
-from dbo.NashvilleHousing
+from portfolio.dbo.Nashville_Housing
 )
 select *
 from row_num
 where RowNum > 1;
 
-
--- deleting unused columns
-
-Alter table portfolio.dbo.NashvilleHousing
-drop column OwnerAddress, PropertyAddress, TaxDistrict,SaleDate
-
 select*
-from dbo.NashvilleHousing
+from portfolio.dbo.Nashville_Housing;
